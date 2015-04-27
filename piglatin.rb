cb500c54c51b5e59f909ec46aa309dce9e6d1ddd
WozniakMac <maciekwo@gmail.com>
2015-04-27 10:50:35 +0200

    Add class

--- piglatin.rb
+++ piglatin.rb
@@ -1,20 +1,18 @@
-def pig_latin(text)
-	text.gsub( /\w+/ ) do |word| 
-		wchars = word.chars
-		first = wchars[0]
-		last = wchars[-1]
-		wchars << first
-		wchars.delete_at(0)
-		wchars[0].upcase! if first == first.upcase
-		wchars[-1].downcase! if last == last.downcase
-		if first.downcase =~ /[eyuioa]/
-			"#{word}way"
-		else
-			"#{wchars.join}ay"
+class PigLatin
+	def self.translate(text)
+		text.gsub( /\w+/ ) do |word| 
+			wchars = word.chars
+			first = wchars[0]
+			last = wchars[-1]
+			wchars << first
+			wchars.delete_at(0)
+			wchars[0].upcase! if first == first.upcase
+			wchars[-1].downcase! if last == last.downcase
+			if first.downcase =~ /[eyuioa]/
+				"#{word}way"
+			else
+				"#{wchars.join}ay"
+			end
 		end
 	end
 end
-puts "Type some text"
-typed = gets
-puts pig_latin(typed)
-gets
